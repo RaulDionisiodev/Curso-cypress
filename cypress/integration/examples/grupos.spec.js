2dea56926448cc289496ee1f5ec2ac39934a02fa
Raul Dionisio <raul.dionisio@voopter.com>
2020-01-06 16:09:08 -0300

    Inserindo primeiros scripst de teste - Aula 2

---
+++ cypress/integration/examples/grupos.spec.js
@@ -0,0 +1,4 @@
+describe("Grupos", () =>{
+    beforeEach(() => cy.visit("http://ondetemgrupo.ga/?i=1"));
+    it("has 'Grupos' header's heading", () =>{});
+});
---
+++ cypress/integration/examples/tickets.spec.js
@@ -0,0 +1,4 @@
+describe("tickets", () => {
+    beforeEach( () => cy.visit("https://bit.ly/2XSuwCW"));
+    it("hs 'TICKETBOX' HEADER'S HEADING", () => {});
+});
